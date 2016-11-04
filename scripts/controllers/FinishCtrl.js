f3713f5736cb6b1659f5e8c6c8c63b0ebcb64e37
Josmario Albuquerque <josmarios.silva@gmail.com>
2016-11-04 00:37:09 -0300

    domain changed: http removed

--- scripts/controllers/FinishCtrl.js
+++ scripts/controllers/FinishCtrl.js
@@ -2,12 +2,12 @@ angular.module('tutor').controller("FinishCtrl", function($scope, User) {
 
     $scope.populationAnxiety = 40.1 * 100 / 80;
     $scope.userAnxiety = User.getAnxiety() * 100 / 80;
-    
+
     $scope.share = function() {
         FB.ui({
             method: 'share',
             display: 'popup',
-            href: 'http://ansiedade.tk/',
+            href: 'ansiedade.tk',
             title: 'Está Ansioso(a)?',
             description: 'Participe deste estudo sobre ansiedade e descubra!',
             picture: 'https://pixabay.com/static/uploads/photo/2016/02/07/14/51/hatena-1184896_960_720.png',
